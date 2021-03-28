3850313ba338f83dd5a6a5d039c8077c7304bc8c
Christine Jeon <csjeon28@gmail.com>
2021-03-28 13:20:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -17,11 +17,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
     if (string.toLowerCase() === string) {
         return ("I can't hear you!");
-        console.log("I can'y hear you!");
     } else if (string.toUpperCase() === string) {
         return ("YES INDEED!");
     } else if (string === "I love you, Grandma.");
         return ("I love you, too.");
     }
-
-console.log(sayHiToGrandma());
